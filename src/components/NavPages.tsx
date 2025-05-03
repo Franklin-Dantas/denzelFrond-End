e869f98d6a6cc896adcce9cac70f9ec81ec30a3c
Antônio Carlos Bezerra Cavalcante Junior <126694183+Carloscavalcante97@users.noreply.github.com>
2025-05-03 17:52:17 -0300

    (FEAT)

--- src/components/NavPages.tsx
+++ src/components/NavPages.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Image from "next/image";
 import Previus from "/public/PreviusIcon.svg";
-import Next from "/public/Next.svg";
+import Next from "/public/next.svg";
 
 interface NavPagesProps {
   page: number;
